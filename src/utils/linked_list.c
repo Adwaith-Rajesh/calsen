f17f954950b9a519576ee4091048d63f566e6f81
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-12 23:00:31 +0530

    Added LinkedList with arena support

---
+++ src/utils/linked_list.c
@@ -0,0 +1,157 @@
+#include "linked_list.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arena.h"
+
+static void *_malloc_with_check(size_t size, Arena *arena) {
+    void *ptr;
+    if (arena == NULL) {
+        ptr = malloc(size);
+    } else {
+        ptr = arena_alloc(arena, size);
+    }
+
+    if (ptr == NULL) {
+        fprintf(stderr, "Could not allocate memory\n");
+        exit(1);
+    }
+    return ptr;
+}
+
+Node *create_node(void *data, Arena *arena) {
+    Node *new_node = (Node *)_malloc_with_check(sizeof(Node), arena);
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void free_node(Node *node) {
+    if (node == NULL) return;
+    free(node);
+}
+
+LinkedList *ll_init(Arena *arena) {
+    LinkedList *new_list = (LinkedList *)_malloc_with_check(sizeof(LinkedList), arena);
+
+    new_list->head = NULL;
+    new_list->size = 0;
+    new_list->arena = arena;
+    return new_list;
+}
+
+void ll_print(LinkedList *list, NodePrinterFn *node_printer) {
+    Node *temp = list->head;
+    while (temp != NULL) {
+        node_printer(temp);
+        temp = temp->next;
+    }
+}
+
+int ll_is_empty(LinkedList *list) {
+    return list->size == 0;
+}
+
+void ll_append(LinkedList *list, Node *node) {
+    if (list->head == NULL) {
+        list->head = node;
+        list->size++;
+        return;
+    }
+
+    Node *temp = list->head;
+    while (temp->next != NULL) temp = temp->next;
+    temp->next = node;
+    list->size++;
+}
+
+Node *ll_pop(LinkedList *list) {
+    if (list->size == 1) {
+        Node *temp = list->head;
+        list->head = NULL;
+        list->size--;
+        return temp;
+    }
+    Node *prev;
+    Node *curr = list->head;
+
+    while (curr->next != NULL) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    prev->next = NULL;
+    list->size--;
+    return curr;
+}
+
+void ll_append_left(LinkedList *list, Node *node) {
+    if (list->head == NULL) {
+        list->head = node;
+        list->size++;
+        return;
+    }
+    node->next = list->head;
+    list->head = node;
+    list->size++;
+}
+
+Node *ll_pop_left(LinkedList *list) {
+    if (list->head == NULL) return NULL;
+    Node *temp = list->head;
+    list->head = list->head->next;
+    list->size--;
+    return temp;
+}
+
+void ll_empty(LinkedList *list) {
+    Node *temp;
+    while (list->head != NULL) {
+        temp = list->head;
+        list->head = list->head->next;
+        free_node(temp);
+    }
+    list->head = NULL;
+    list->size = 0;
+}
+
+void ll_free(LinkedList *list) {
+    Node *temp;
+    while (list->head != NULL) {
+        temp = list->head;
+        list->head = list->head->next;
+        free_node(temp);
+    }
+    free(list);
+}
+
+LLIter *ll_iter_init(LinkedList *list) {
+    LLIter *new_iter = (LLIter *)_malloc_with_check(sizeof(LLIter), list->arena);
+    new_iter->curr = list->head;
+    return new_iter;
+}
+
+Node *ll_iter_next(LLIter *iterator) {
+    if (iterator->curr == NULL) return NULL;
+    Node *curr_val = iterator->curr;
+    iterator->curr = iterator->curr->next;
+    return curr_val;
+}
+
+void ll_iter_free(LLIter *iterator) {
+    if (iterator == NULL) return;
+    free(iterator);
+}
+
+void ll_map(LinkedList *list, LinkedListMapFn *fn, ...) {
+    Node *temp = list->head;
+    va_list args;
+    va_start(args, fn);
+    while (temp != NULL) {
+        temp->data = fn(temp, args);
+        temp = temp->next;
+    }
+    va_end(args);
+}
---
+++ src/utils/linked_list.h
@@ -0,0 +1,45 @@
+#ifndef LIBS_INCLUDE_LINKED_LIST_H
+#define LIBS_INCLUDE_LINKED_LIST_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "arena.h"
+
+typedef struct Node {
+    void *data;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    Node *head;
+    size_t size;
+    Arena *arena;
+} LinkedList;
+
+typedef struct {
+    Node *curr;
+} LLIter;
+
+typedef void NodePrinterFn(Node *);
+typedef void *LinkedListMapFn(Node *, va_list);
+
+Node *create_node(void *, Arena *);
+void free_node(Node *);
+
+LinkedList *ll_init(Arena *);
+void ll_print(LinkedList *, NodePrinterFn *);
+int ll_is_empty(LinkedList *);
+void ll_append(LinkedList *, Node *);
+Node *ll_pop(LinkedList *);
+void ll_append_left(LinkedList *, Node *);
+Node *ll_pop_left(LinkedList *);
+void ll_map(LinkedList *, LinkedListMapFn *, ...);
+void ll_empty(LinkedList *);
+void ll_free(LinkedList *);
+
+LLIter *ll_iter_init(LinkedList *);
+void ll_iter_free(LLIter *);
+Node *ll_iter_next(LLIter *);
+
+#endif
