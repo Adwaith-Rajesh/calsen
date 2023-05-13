f509f507f99deb19a4e61eb0e0d03fc249c90b2c
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-13 17:12:22 +0530

    Begin test/plain parser

---
+++ src/parsers/text_plain.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cstring.h"
+
+static char *_read_file(const char *path) {
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+String *parse_file(const char *pathname) {
+    printf("hello World\n");
+    (void)pathname;
+    return NULL;
+}
+#pragma GCC diagnostic pop
