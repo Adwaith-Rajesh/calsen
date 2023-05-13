9ef0c8a36a4c85efc18fbd20db0e7f21c51efad2
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-13 10:51:38 +0530

    Updated the build script to include parsers dir

--- nobuild.c
+++ nobuild.c
@@ -10,6 +10,11 @@
 #define OUT_DIR "./build/out"
 #define BIN_DIR "./build/bin"
 
+typedef struct {
+    char parser_file_name[20];
+    Cmd cmd;
+} CParserCompileCommand;  // custom parser compile command
+
 static void _build_object_file(const char *file, const char *o_path) {
     CMD("cc", C_FLAGS, "-o",
         PATH("build", "out", CONCAT(NOEXT(file), ".o")),
@@ -48,6 +53,7 @@ void build_src() {
 }
 
 void build_calsen() {
+    INFO("Building Calsen");
     const char *out_dir = "./build/out";
     const char *bin_dir = "./build/bin";
 
@@ -61,16 +67,65 @@ void build_calsen() {
     });
     Cmd cmd = {
         .line = line};
+    INFO("CMD: %s", cmd_show(cmd));
     cmd_run_sync(cmd);
+}
+
+int custom_parser_check_execute(Cstr file) {
+    CParserCompileCommand cp_commands[] = {
+        // sample code
+        // {
+        //     .parser_file_name = "text_plain.c",
+        //     .cmd = (Cmd){
+        //         .line = cstr_array_make("cc", C_FLAGS, "-c",  //
+        //                                 PATH("src", "parsers", "text_plain.c"), "-o",
+        //                                 PATH("build", "parsers", "text_plain.o"),
+        //                                 NULL),
+        //     },
+        // },
+    };
+
+    size_t cp_commands_len = sizeof(cp_commands) / sizeof(CParserCompileCommand);
+
+    if (!cp_commands_len) return 0;
+
+    for (size_t i = 0; i < cp_commands_len; i++) {
+        if (strcmp(file, cp_commands[i].parser_file_name) == 0) {
+            INFO("CMD: %s", cmd_show(cp_commands[i].cmd));
+            cmd_run_sync(cp_commands[i].cmd);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void build_parsers() {
+    INFO("Building parsers");
+    if (!IS_DIR("./src/parsers")) {
+        ERRO("parser directory does not exist");
+        return;
+    }
+    FOREACH_FILE_IN_DIR(file, "./src/parsers", {
+        if (ENDS_WITH(file, ".c")) {
+            if (!custom_parser_check_execute(file)) {
+                CMD("cc", C_FLAGS, "-shared", "-fPIC", "-o",
+                    PATH("build", "parsers", CONCAT(NOEXT(file), ".so")),
+                    PATH(OUT_DIR, "arena.o"),
+                    PATH(OUT_DIR, "cstring.o"));
+            }
+        }
+    });
 }
 
 int main(int argc, char **argv) {
     MKDIRS("build", "out");
     MKDIRS("build", "bin");
+    MKDIRS("build", "parsers");
 
     GO_REBUILD_URSELF(argc, argv);
     build_src();
     build_src_utils();
     build_calsen();
+    build_parsers();
     return EXIT_SUCCESS;
 }
