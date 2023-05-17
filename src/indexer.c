ea90b04a0eda405e2d816abc0316e2688f307556
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-05-17 23:23:26 +0530

    added a function to increase the size of a string if it reaches the size limit

--- src/indexer.c
+++ src/indexer.c
@@ -129,7 +129,24 @@ key, "points" to another hash table where each key is token with its tf value
 
 /* =============== Load the contents from the file =============== */
 
+// check if the char can be appended, otherwise create a new string
+// with double the size. It' static as indexer might be the only place where this is needed
+static String *_string_expandable_append(String *str, char c) {
+    if (str->curr_p < str->size - 1) {
+        string_append_char(str, c);
+        return str;
+    } else {
+        String *new_string = string_create_from_charp(str->str, str->size * 2);
+        string_destroy(str);
+        return new_string;
+    }
+    return NULL;
+}
+
 HashTable *load_index(const char *index_file) {
+    // options
+    // 1. parse character by char
+    // 2. read an entire line, then parse
 }
 
 /* =============== Dump the contents to a file =============== */
