1562bb217b12e7ab328ef55d612464e8ce1617c2
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-06-09 16:37:37 +0530

    Updated the header files for the util libs

--- src/utils/cstring.h
+++ src/utils/cstring.h
@@ -14,16 +14,16 @@ typedef struct {
     size_t curr_p;
 } String;
 
-String *string_create(size_t);
-void string_destroy(String *);
-void string_append_char(String *, char);
-void string_append_charp(String *, const char *);
-void string_print(String *);
-String *string_create_from_charp(const char *, size_t);
-String *string_create_from_charp_slice(CharPSlice *);
-void charp_slice_to_charp_array(CharPSlice *, char[]);
-void charp_slice_print(CharPSlice *);
-void string_reset(String *);
-String *string_expandable_append(String *, char c);
+String *string_create(size_t size);
+void string_destroy(String *string);
+void string_append_char(String *string, char ch);
+void string_append_charp(String *string, const char *charp);
+void string_print(String *string);
+String *string_create_from_charp(const char *str, size_t size);
+String *string_create_from_charp_slice(CharPSlice *slice);
+void charp_slice_to_charp_array(CharPSlice *slice, char arr[]);
+void charp_slice_print(CharPSlice *slice);
+void string_reset(String *string);
+String *string_expandable_append(String *str, char c);
 
 #endif
--- src/utils/path.h
+++ src/utils/path.h
@@ -1,8 +1,8 @@
 #ifndef _INCLUDE_PATH_H
 #define _INCLUDE_PATH_H
 
-int is_dir(const char *);
-char *get_absolute_path(const char *, char *);
-int get_file_size(const char *);
+int is_dir(const char *pathname);
+char *get_absolute_path(const char path *, char *resolved_path);
+int get_file_size(const char *path);
 
 #endif
