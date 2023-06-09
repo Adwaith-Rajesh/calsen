3c987538ac63e7a4532f2dbb198027cc14bf7b4d
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-06-09 17:00:18 +0530

    replace malloc with malloc_with_check for util functions

--- src/utils/path.h
+++ src/utils/path.h
@@ -2,7 +2,7 @@
 #define _INCLUDE_PATH_H
 
 int is_dir(const char *pathname);
-char *get_absolute_path(const char path *, char *resolved_path);
+char *get_absolute_path(const char *path, char *resolved_path);
 int get_file_size(const char *path);
 
 #endif
